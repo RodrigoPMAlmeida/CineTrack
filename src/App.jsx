1140c9af0f1f6fce1b5eba97ca2503614ba63326
matus014 <mateus_o_gomes@estudante.sesisenai.org.br>
2025-03-21 15:26:06 -0300

    tela de cadastro front

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import './App.css'
+import Teste from'./componets/Cadastrar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Teste></Teste>
+    </>
+  )
+}
+
+export default App
